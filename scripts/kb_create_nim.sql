7495aeaf73be7022c9efe4fde9df2c9061955868
Vir-Sikand <sikandvir@gmail.com>
2025-10-11 19:30:27 -0700

    changed and database insert works

--- scripts/kb_create_nim.sql
+++ scripts/kb_create_nim.sql
@@ -8,18 +8,26 @@ PARAMETERS = {
   "password": "kb_pass_123",
   "distance": "cosine"
 };
-DROP KNOWLEDGE_BASE IF EXISTS org_kb_nim;
-CREATE KNOWLEDGE_BASE org_kb_nim
+
+CREATE ML_ENGINE nim_openai
+FROM openai
+USING
+  api_key = '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',
+  base_url = 'https://integrate.api.nvidia.com/v1';
+
+DROP KNOWLEDGE_BASE IF EXISTS org_kb_openai;
+CREATE KNOWLEDGE_BASE org_kb_openai
 USING
   storage = org_pg.storage_table,
   embedding_model = {
     "provider": "openai",
-    "base_url": "https://integrate.api.nvidia.com/v1",
-    "model_name": "nvidia/nv-embedqa-e5-v5",
-    "api_key": "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"
+    "model_name": "text-embedding-3-small",  
+    "api_key": "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"
   },
   content_columns  = ['text'],
   metadata_columns = ['title','page','source_uri'];
-INSERT INTO org_kb_nim (text, title, page, source_uri)
+
+INSERT INTO org_kb_openai (text, title, page, source_uri)
 VALUES ('Hello vectors from NIM!', 'Sanity', 1, 'local');
-SELECT * FROM org_kb_nim WHERE content = 'hello vectors';
+SELECT * FROM org_kb_openai WHERE content = 'hello vectors';
+
